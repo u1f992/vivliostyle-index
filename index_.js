f1ab24b4fa322f402ba1a0a8be20ded2139946df
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-05-29 16:54:30 +0900

    Update index_.js

--- index_.js
+++ index_.js
@@ -1,5 +1,8 @@
 // @ts-check
 
+import getXPath from "get-xpath";
+import { base16 } from "rfc4648";
+
 import {
   getRegisterPageLocatorToEntryQueryValidator,
   getRegisterRangeLocatorToEntryQueryValidator,
@@ -16,16 +19,11 @@ import {
 } from "./query-validators.js";
 
 /**
- * @typedef {{
- *   type: string;
- *   file: string;
- *   id: string | [string, string];
- * }} LocatorBase
+ * @typedef {{ file: string }} LocatorBase
  * @typedef {LocatorBase & { type: "page"; id: string }} PageLocator
  * @typedef {LocatorBase & { type: "range"; id: [string, string] }} RangeLocator
  *
  * @typedef {{
- *   type: string;
  *   group: string;
  *   reading: string;
  *   headword: string;
@@ -54,53 +52,338 @@ import {
 
 /**
  * @param {Indexes} indexes
- * @param {any} query
+ * @param {string} indexId
+ * @param {string} groupTag
+ * @param {string} reading
+ * @param {string} headword
+ */
+function ensureTargetEntry(indexes, indexId, groupTag, reading, headword) {
+  const targetIndex = indexes[indexId];
+  const targetGroup =
+    targetIndex.find((g) => g.group === groupTag) ??
+    targetIndex[targetIndex.push({ group: groupTag, entries: [] }) - 1];
+  const targetEntry =
+    targetGroup.entries.find(
+      (e) => e.reading === reading && e.headword === headword,
+    ) ??
+    targetGroup.entries[
+      targetGroup.entries.push({
+        reading,
+        headword,
+        locators: [],
+        see: [],
+        seeAlso: [],
+        subentries: [],
+      }) - 1
+    ];
+  return targetEntry;
+}
+
+/**
+ * @param {Indexes} indexes
+ * @param {string} indexId
+ * @param {string} groupTag
+ * @param {string} reading
+ * @param {string} headword
+ * @param {string} subReading
+ * @param {string} subHeadword
+ */
+function ensureTargetSubentry(
+  indexes,
+  indexId,
+  groupTag,
+  reading,
+  headword,
+  subReading,
+  subHeadword,
+) {
+  const targetEntry = ensureTargetEntry(
+    indexes,
+    indexId,
+    groupTag,
+    reading,
+    headword,
+  );
+  const targetSubentry =
+    targetEntry.subentries.find(
+      (se) => se.reading === subReading && se.headword === subHeadword,
+    ) ??
+    targetEntry.subentries[
+      targetEntry.subentries.push({
+        reading,
+        headword,
+        locators: [],
+        see: [],
+        seeAlso: [],
+      }) - 1
+    ];
+  return targetSubentry;
+}
+
+/**
  * @param {HTMLElement} elem
  */
-function register(indexes, query, elem) {
-  if (getRegisterPageLocatorToEntryQueryValidator()(query)) {
-    const [id, [group, reading_, headword_]] = query[0];
-    const reading = reading_ ?? elem.innerHTML;
-    const headword = headword_ ?? elem.innerHTML;
+function ensureId(elem) {
+  const id = elem.hasAttribute("id")
+    ? (elem.getAttribute("id") ?? "")
+    : base16.stringify(new TextEncoder().encode(getXPath(elem)));
+  if (!elem.hasAttribute("id")) {
+    elem.setAttribute("id", id);
+  }
+  return id;
+}
 
-    const targetIndex = indexes[id];
-    const targetGroup =
-      targetIndex.find((g) => g.group === group) ??
-      targetIndex[targetIndex.push({ group, entries: [] }) - 1];
-    const targetEntry =
-      targetGroup.entries.find(
-        (e) => e.reading === reading && e.headword === headword,
-      ) ??
-      targetGroup.entries[
-        targetGroup.entries.push({
-          reading,
-          headword,
-          locators: [],
-          see: [],
-          seeAlso: [],
-          subentries: [],
-        }) - 1
-      ];
-    targetEntry.locators.push({ type: "page", file: "", id: "" });
+/**
+ * @param {HTMLElement} elem
+ */
+function appendSpanAsLastChild(elem) {
+  const span = document.createElement("span");
+  elem.appendChild(span);
+  return span;
+}
+
+/**
+ * @param {Indexes} indexes
+ * @param {any} query
+ * @param {string} file
+ * @param {HTMLElement} elem
+ */
+export function register(indexes, query, file, elem) {
+  if (getRegisterPageLocatorToEntryQueryValidator()(query)) {
+    const [indexId, [group, reading, headword]] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).locators.push({
+      type: "page",
+      file,
+      id: ensureId(elem),
+    });
   } else if (getRegisterRangeLocatorToEntryQueryValidator()(query)) {
+    const [indexId, [group, reading, headword], _] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).locators.push({
+      type: "range",
+      file,
+      id: [ensureId(elem), ensureId(appendSpanAsLastChild(elem))],
+    });
   } else if (getRegisterSeeReferenceOfEntryToEntryQueryValidator()(query)) {
+    const [
+      indexId,
+      [group, reading, headword],
+      _,
+      [refGroup, refReading, refHeadword],
+    ] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).see.push({
+      type: "entry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+    });
   } else if (getRegisterSeeReferenceOfSubentryToEntryQueryValidator()(query)) {
+    const [
+      indexId,
+      [group, reading, headword],
+      _,
+      [[refGroup, refReading, refHeadword], refSubReading, refSubHeadword],
+    ] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).see.push({
+      type: "subentry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+      subReading: refSubReading ?? elem.innerHTML,
+      subHeadword: refSubHeadword ?? elem.innerHTML,
+    });
   } else if (getRegisterSeeAlsoReferenceOfEntryToEntryQueryValidator()(query)) {
+    const [
+      indexId,
+      [group, reading, headword],
+      _,
+      [refGroup, refReading, refHeadword],
+    ] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).seeAlso.push({
+      type: "entry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+    });
   } else if (
     getRegisterSeeAlsoReferenceOfSubentryToEntryQueryValidator()(query)
   ) {
+    const [
+      indexId,
+      [group, reading, headword],
+      _,
+      [[refGroup, refReading, refHeadword], refSubReading, refSubHeadword],
+    ] = query;
+    ensureTargetEntry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+    ).seeAlso.push({
+      type: "subentry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+      subReading: refSubReading ?? elem.innerHTML,
+      subHeadword: refSubHeadword ?? elem.innerHTML,
+    });
   } else if (getRegisterPageLocatorToSubentryQueryValidator()(query)) {
+    const [indexId, [[group, reading, headword], subReading, subHeadword]] =
+      query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).locators.push({
+      type: "page",
+      file,
+      id: ensureId(elem),
+    });
   } else if (getRegisterRangeLocatorToSubentryQueryValidator()(query)) {
+    const [indexId, [[group, reading, headword], subReading, subHeadword], _] =
+      query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).locators.push({
+      type: "range",
+      file,
+      id: [ensureId(elem), ensureId(appendSpanAsLastChild(elem))],
+    });
   } else if (getRegisterSeeReferenceOfEntryToSubentryQueryValidator()(query)) {
+    const [
+      indexId,
+      [[group, reading, headword], subReading, subHeadword],
+      _,
+      [refGroup, refReading, refHeadword],
+    ] = query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).see.push({
+      type: "entry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+    });
   } else if (
     getRegisterSeeReferenceOfSubentryToSubentryQueryValidator()(query)
   ) {
+    const [
+      indexId,
+      [[group, reading, headword], subReading, subHeadword],
+      _,
+      [[refGroup, refReading, refHeadword], refSubReading, refSubHeadword],
+    ] = query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).see.push({
+      type: "subentry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+      subReading: refSubReading ?? elem.innerHTML,
+      subHeadword: refSubHeadword ?? elem.innerHTML,
+    });
   } else if (
     getRegisterSeeAlsoReferenceOfEntryToSubentryQueryValidator()(query)
   ) {
+    const [
+      indexId,
+      [[group, reading, headword], subReading, subHeadword],
+      _,
+      [refGroup, refReading, refHeadword],
+    ] = query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).seeAlso.push({
+      type: "entry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+    });
   } else if (
     getRegisterSeeAlsoReferenceOfSubentryToSubentryQueryValidator()(query)
   ) {
+    const [
+      indexId,
+      [[group, reading, headword], subReading, subHeadword],
+      _,
+      [[refGroup, refReading, refHeadword], refSubReading, refSubHeadword],
+    ] = query;
+    ensureTargetSubentry(
+      indexes,
+      indexId,
+      group,
+      reading ?? elem.innerHTML,
+      headword ?? elem.innerHTML,
+      subReading ?? elem.innerHTML,
+      subHeadword ?? elem.innerHTML,
+    ).seeAlso.push({
+      type: "subentry",
+      group: refGroup,
+      reading: refReading ?? elem.innerHTML,
+      headword: refHeadword ?? elem.innerHTML,
+      subReading: refSubReading ?? elem.innerHTML,
+      subHeadword: refSubHeadword ?? elem.innerHTML,
+    });
   } else {
+    throw new Error(`incorrect query format: ${JSON.stringify(query)}`);
   }
 }
