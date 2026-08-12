c9a6124223dc98ee301a88d64bed2f07c14ea2fe
Koutaro Mukai <mukai.k1011k@outlook.jp>
2026-08-12 22:08:28 +0900

    Remove unused error helper

--- src/util.ts
+++
@@ -1,7 +0,0 @@
-type CtorWith<TArgs extends unknown[]> = new (...args: TArgs) => Error;
-export function throwError<TArgs extends unknown[]>(
-  args: TArgs,
-  { ctor }: { ctor?: CtorWith<TArgs> } = {},
-): never {
-  throw new ((ctor ?? Error) as CtorWith<TArgs>)(...args);
-}
