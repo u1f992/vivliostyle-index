f6925de4c422194beb0c4eb4e0c560ccdfdefa8d
Koutaro Mukai <mukai.k1011k@outlook.jp>
2026-08-18 14:53:01 +0900

    Align basic example filenames with page ranges

--- examples/basic/vivliostyle.config.ts
+++ examples/basic/vivliostyle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, readMetadata, VFM } from "@vivliostyle/cli";
 import { createIndexPlugin, logMessages } from "@u1f992/vivliostyle-index";
 
-const entry = ["001-050.md", "051-099.md", "index.md", "100-150.md"];
+const entry = ["001-050.md", "051-097.md", "098-099-index.md", "100-150.md"];
 const index = createIndexPlugin({ entry });
 
 export default defineConfig({
